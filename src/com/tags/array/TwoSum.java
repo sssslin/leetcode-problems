c88f89cb04c91ecb331619b396028c0ccad588bb
sssslin <414048771@qq.com>
2020-05-05 20:14:24 +0800

    注释

--- src/com/tags/array/TwoSum.java
+++ src/com/tags/array/TwoSum.java
@@ -12,6 +12,7 @@ public class TwoSum {
         }
     }
 
+    // 个人第一次的写法,用的是最简单的遍历
     public static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
