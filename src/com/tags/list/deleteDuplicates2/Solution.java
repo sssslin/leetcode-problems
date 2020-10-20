382923225213b042855ea0ab197e0847aaecda82
sssslin <414048771@qq.com>
2020-10-20 17:29:29 +0800

    commit:题目模板提交

---
+++ src/com/tags/list/deleteDuplicates2/Solution.java
@@ -0,0 +1,25 @@
+package com.tags.list.deleteDuplicates2;
+
+import test.BaseSolution;
+import test.ListNode;
+
+//给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
+//
+// 示例 1:
+//
+// 输入: 1->2->3->3->4->4->5
+//输出: 1->2->5
+//
+//
+// 示例 2:
+//
+// 输入: 1->1->1->2->3
+//输出: 2->3
+// Related Topics 链表
+// 👍 384 👎 0
+public class Solution extends BaseSolution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        return head;
+    }
+}
