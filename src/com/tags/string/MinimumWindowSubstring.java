183d9de73604597228556c5d2d9d389768e5ec82
sssslin <414048771@qq.com>
2021-05-29 16:07:36 +0800

    commit:滑动窗口76---最小覆盖子串细节修改

--- src/com/tags/string/MinimumWindowSubstring.java
+++ src/com/tags/string/MinimumWindowSubstring.java
@@ -7,20 +7,20 @@ import java.util.Map;
  * 来offer：https://www.youtube.com/watch?v=9qFR2WQGqkU
  * 1. corner case
  * 2. 初始化变量：
- *    2.1. slow指针
- *    2.2. minLen即最终需要返回的值
- *    2.3. 将子串的字符存入一个map,key:字符 ,value : 字符需要匹配的次数
- *   2. 4. matchCount匹配是否存在可行解，当map.size() = matchCount则说明存在可行解
- *   2. 5. fast指针进行外层循环，寻找是否存在可行解，直到map.size() =  matchCount时slow指针开始移动,
- *       外层循环是一个matchCount++的过程，内层循环是matchCount--的过程，当matchCount<map.size()说明当前窗口已经不符合要求，右端点重新开始移动。直到右端点到达右边界，循环停止
+ * 2.1. slow指针
+ * 2.2. minLen即最终需要返回的值
+ * 2.3. 将子串的字符存入一个map,key:字符 ,value : 字符需要匹配的次数
+ * 2. 4. matchCount匹配是否存在可行解，当map.size() = matchCount则说明存在可行解
+ * 2. 5. fast指针进行外层循环，寻找是否存在可行解，直到map.size() =  matchCount时slow指针开始移动,
+ * 外层循环是一个matchCount++的过程，内层循环是matchCount--的过程，当matchCount<map.size()说明当前窗口已经不符合要求，右端点重新开始移动。直到右端点到达右边界，循环停止
  * 3. 判断minLen是否被更新过，如果没被更新过，则返回默认值
- *
- * 难点：边界情况、特殊情况的处理
+ * <p>
+ * 难点：当一个字符出现多次的时候，该如何处理的问题
  */
 public class MinimumWindowSubstring {
 
     // Assumption: s and t are both not null
-    public String minWindow(String s, String t) {
+    public static String minWindow(String s, String t) {
         // corner case:
         if (s.length() < t.length()) {
             return "";
@@ -40,7 +40,7 @@ public class MinimumWindowSubstring {
             }
             wordDict.put(ch, count - 1);
 
-            // TODO: 2021/5/27 这里这个if操作不是很理解
+            // count - 1并不影响当前的值
             // match another character
             if (count == 1) {
                 // 1 -> 0
@@ -54,7 +54,7 @@ public class MinimumWindowSubstring {
                     index = slow;
                 }
 
-                // TODO: 2021/5/27 这里这个if操作不是很理解
+                System.out.println(slow);
                 char leftMost = s.charAt(slow++);
                 Integer leftmostCount = wordDict.get(leftMost);
                 if (leftmostCount == null) {
@@ -66,8 +66,6 @@ public class MinimumWindowSubstring {
                     // 0 -> 1
                     matchCount--;
                 }
-
-
             }
         }
         return minLen == Integer.MAX_VALUE ? "" : s.substring(index, index + minLen);
@@ -76,10 +74,11 @@ public class MinimumWindowSubstring {
     /**
      * 用t里面的元素构造成一个map
      * 构造逻辑：map中不存在当前字符，则value设为1；如果map中存在当前字符，则value++
+     *
      * @param t
      * @return
      */
-    private Map<Character, Integer> constructWordDict(String t) {
+    private static Map<Character, Integer> constructWordDict(String t) {
         Map<Character, Integer> map = new HashMap<>();
         for (char ch : t.toCharArray()) {
             Integer count = map.get(ch);
@@ -92,5 +91,11 @@ public class MinimumWindowSubstring {
         return map;
     }
 
+    public static void main(String[] args) {
+        String s = "aa";
+        String t = "aa";
 
+        String result = minWindow(s, t);
+        System.out.println(result);
+    }
 }
